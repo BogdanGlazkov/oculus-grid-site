11cd1b3fc1db8e38193490abfd21cfedf65a1f82
BogdanGlazkov <bglazkov@i.ua>
2023-06-11 20:17:52 +0300

    Add features slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-$(function () {});
+"use strict";
+
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", openContent);
+});
+
+function openContent(e) {
+  const tabTarget = e.currentTarget;
+  const btnTarget = tabTarget.dataset.button;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item--active");
+  });
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item--active");
+  });
+
+  tabTarget.classList.add("tabs__btn-item--active");
+  document
+    .querySelector(`#${btnTarget}`)
+    .classList.add("tabs__content-item--active");
+}
